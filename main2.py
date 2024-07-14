294915e75aae4d1fe67fbc6fba7b3e25d0b1cfd4
Arjun Singh Rajput <149608752+ArjunRajputGLA@users.noreply.github.com>
2024-07-14 12:29:45 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -63,7 +63,7 @@ st.markdown(
         padding: 0.5em 1em !important;
     }
     .sidebar-video {
-        margin-top: -50px;
+        margin-top: -60px;
         margin-bottom: 30px;
         margin-left: -1rem;
         margin-right: -1rem;
